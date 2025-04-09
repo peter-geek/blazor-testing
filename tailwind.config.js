7a849133e029d2703c5c88fe3583fa7946cf7bcd
Peter <coderscave1@gmail.com>
2025-04-09 16:38:57 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./Pages/**/*.razor", "./Components/**/*.razor", "./Shared/**/*.razor", "./**/*.html"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
